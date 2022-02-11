2de9097f8142960b9fbc8e404e6e41b0d834a498
Airsleep <green3438@gmail.com>
2022-02-11 17:07:10 +0900

    app.js modified
    Adding saving canvas function.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "2c2c2c";
 //Canvas Initial Values
@@ -14,6 +15,11 @@ canvas.height = 500;
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.lineWidth = 1;
 
+//Canvas Backgroung Color Setting
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+//Canvas filling Initial Values;
 ctx.fillStyle = INITIAL_COLOR;
 
 let painting = false;
@@ -66,12 +72,25 @@ function handleCanvasClick() {
   }
 }
 
+function handleContextMenu(event) {
+  event.preventDefault();
+}
+
+function handleSaveClick() {
+  const image = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "nomad_paint_clone[EXPORT]";
+  link.click();
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleContextMenu);
 }
 
 Array.from(colors).forEach((color) =>
@@ -84,4 +103,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleModeClicked);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
